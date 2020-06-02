2b4f16fe8a531b4a72a206649a099c0ddd9b006b
Lucas8x <lucas.poring99@gmail.com>
2020-06-02 12:55:26 -0300

    Backend

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routees from './routes';
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(routees);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333);
